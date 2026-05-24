170055d8b0311b8e5335d7fdcbcd753afbe6e170
Marioo-Jr <mario.junior23@univille.br>
2026-05-24 16:15:07 -0300

    Audit data

---
+++ BackEnd/src/main/java/com/ProductCatalog/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.ProductCatalog.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+
+}
